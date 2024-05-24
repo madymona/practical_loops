160ed4dc315030368416b1844a988e5602d33fab
madymona <madymona@gmail.com>
2024-05-24 14:40:55 -0400

    added Part 3

--- ALAB_308.3.1.js
+++ ALAB_308.3.1.js
@@ -44,3 +44,21 @@ while (true) {
 
     num++
 }
+
+
+//Feeling Loopy
+
+const csv ='ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+//Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+
+
+const rows = csv.split('\n');
+
+for (row of rows) {
+  const cells = row.split(',');
+  const cell1 = cells[0];
+  const cell2 = cells[1];
+  const cell3 = cells[2];
+  const cell4 = cells[3];
+  console.log(cell1, cell2, cell3, cell4);
+}
